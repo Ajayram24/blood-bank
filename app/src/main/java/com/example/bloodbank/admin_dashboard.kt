b1f2bb245786ad7d66789cf342c0e5688f44bf30
Ajayram24 <ramajay2004@gmail.com>
2025-07-31 13:35:14 +0530

    3 PAGE

--- app/src/main/java/com/example/bloodbank/admin_dashboard.kt
+++ app/src/main/java/com/example/bloodbank/admin_dashboard.kt
@@ -16,5 +16,7 @@ class admin_dashboard : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
